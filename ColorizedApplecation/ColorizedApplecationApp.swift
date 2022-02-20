c545cff1a7104b7af8fd832367296e9ee4cd6de2
AL-Lina <lina-sokolova00@mail.ru>
2022-02-20 19:02:47 +0300

    Initial Commit

---
+++ ColorizedApplecation/ColorizedApplecationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApplecationApp.swift
+//  ColorizedApplecation
+//
+//  Created by Alina Sokolova on 20.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApplecationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApplecation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorizedApplecation
+//
+//  Created by Alina Sokolova on 20.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
